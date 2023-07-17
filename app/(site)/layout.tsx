8ed8bf0b3bc6afc3f76d389811bfc25c7da1aa32
ajithdev-c <ajithdev.c@grainmetrics.com>
2023-07-17 16:22:55 +0530

    revalidate false

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -2,7 +2,7 @@ import '../globals.css'
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
 import { getPages } from '@/sanity/sanity-utils'
-export const dynamic = 'force-dynamic'
+export const revalidate = false
 
 
 const inter = Inter({ subsets: ['latin'] })
