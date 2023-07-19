312dd64ffeeab1e9ac712d0728040cfda1ea1996
ajithdev-c <ajithdev.c@grainmetrics.com>
2023-07-19 17:53:37 +0530

    te

--- app/(site)/api/revalidate/route.ts
+++ app/(site)/api/revalidate/route.ts
@@ -1,8 +1,12 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { revalidatePath } from 'next/cache'
- 
-export async function GET(request: NextRequest) {
-  const path = request.nextUrl.searchParams.get('/team') || '/'
-  revalidatePath(path)
-  return NextResponse.json({ revalidated: true, now: Date.now() })
+import { NextApiResponse } from "next";
+
+export default async function handler( res: NextApiResponse) {
+    try {
+
+        await res.revalidate('/team');
+        return res.json({ revalidated: true });
+
+    } catch (err) {
+        return res.status(500).send('Error revalidating');
+    }
 }
