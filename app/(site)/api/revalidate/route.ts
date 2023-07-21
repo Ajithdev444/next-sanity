813142e6341c65fe281bcb14250cd7e991a13f7b
ajithdev-c <ajithdev.c@grainmetrics.com>
2023-07-21 11:10:53 +0530

    C:/Program Files/Git/team

--- app/(site)/api/revalidate/route.ts
+++ app/(site)/api/revalidate/route.ts
@@ -3,7 +3,7 @@ import { revalidatePath } from 'next/cache'
  
  
 export async function GET(request: NextRequest) {
-  const path = request.nextUrl.searchParams.get('team') || '/'
+  const path = request.nextUrl.searchParams.get('/team') || '/'
   revalidatePath(path)
   return NextResponse.json({ revalidated: true, now: Date.now() })
 }
