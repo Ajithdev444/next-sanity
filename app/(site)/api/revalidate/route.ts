64b6f2c91972ba0208cb586f6ca1adf911ab7254
ajithdev-c <ajithdev.c@grainmetrics.com>
2023-07-21 13:02:55 +0530

    gallery

--- app/(site)/api/revalidate/route.ts
+++ app/(site)/api/revalidate/route.ts
@@ -6,5 +6,7 @@ export async function GET(request: NextRequest) {
   console.log(request)
   const path = request.nextUrl.searchParams.get('/team') || '/'
   revalidatePath(path)
+  const gallery = request.nextUrl.searchParams.get('/gallery') || '/'
+  revalidatePath(gallery)
   return NextResponse.json({ revalidated: true, now: Date.now() })
 }
